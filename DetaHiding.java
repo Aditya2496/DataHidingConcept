0482ec853130baa540050b1aea3f92b39d69d372
Aditya2496 <adityaanilkute2441996@gmail.com>
2021-09-08 19:20:59 +0530

    cahnges Required

---
+++ DetaHiding.java
@@ -0,0 +1,15 @@
+package com.mainmethoddemo;
+
+public class DetaHiding {
+	
+	public static void main(String[] args) {
+		System.out.println("Shrutika");
+	}
+	private static void main(String[] args) {
+		System.out.println("Rucha");
+	}
+	public static void main2(String[] args) {
+		System.out.println("Aditya");
+	}
+
+}
