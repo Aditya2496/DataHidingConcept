0d448647b6efcf2d3d264be601e467f993fc4273
Aditya2496 <adityaanilkute2441996@gmail.com>
2021-09-09 15:19:48 +0530

    Remove method

--- DetaHiding.java
+++ DetaHiding.java
@@ -8,8 +8,5 @@ public class DetaHiding {
 	private static void main(String[] args) {
 		System.out.println("Rucha");
 	}
-	public static void main2(String[] args) {
-		System.out.println("Aditya Kute");
-	}
-
+	
 }
